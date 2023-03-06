db75bcf4c3fd766b0f27fff44fdb1aa0e91276e7
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-03-06 09:56:39 -0300

    se agrega el ItemViewHolder como clase dentro del adapter

--- app/src/main/java/com/example/recyclerviewejemplo/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerviewejemplo/adapter/ItemAdapter.kt
@@ -1,8 +1,14 @@
 package com.example.recyclerviewejemplo.adapter
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewejemplo.R
 import com.example.recyclerviewejemplo.model.Affirmation
 
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
-
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
 }
