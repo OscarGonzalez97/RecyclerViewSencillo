fd9472877a84132401b221685a60d85a3c8dd9d7
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-03-06 09:46:24 -0300

    ItemAdapter necesita información sobre cómo resolver los recursos de strings. Esta información sobre la app se almacena en una instancia del objeto Context

--- app/src/main/java/com/example/recyclerviewejemplo/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerviewejemplo/adapter/ItemAdapter.kt
@@ -1,7 +1,8 @@
 package com.example.recyclerviewejemplo.adapter
 
+import android.content.Context
 import com.example.recyclerviewejemplo.model.Affirmation
 
-class ItemAdapter(private val dataset: List<Affirmation>) {
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
 
 }
