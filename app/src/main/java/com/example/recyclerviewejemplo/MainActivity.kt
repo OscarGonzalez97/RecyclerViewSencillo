e25aa5405c1830a27716b1e3dd5d9e8af1ed105d
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-03-06 10:24:53 -0300

    se pone los datos en el recyclerView y se pone el adapter correspondiente

--- app/src/main/java/com/example/recyclerviewejemplo/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewejemplo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.recyclerviewejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewejemplo.adapter.ItemAdapter
+import com.example.recyclerviewejemplo.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
     }
 }
